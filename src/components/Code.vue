cd8bf2d85bb55b742cf90608a093c5d2e66be69a
Michael Thiessen <electromikenetic@gmail.com>
2020-02-26 14:05:48 -0500

    clean up lots of stuff

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,5 +1,11 @@
 <template>
-  <pre ref="code" class="code-container language-js"><transition-group name="list" tag="div"><code class="list-item" v-for="change in html" :key="change.id" v-html="change.value + '\n'"/></transition-group></pre>
+  <pre ref="code" class="code-container language-js"><transition-group name="list" tag="div"><code
+  :ref="change.id"
+  class="list-item"
+  v-for="change in html"
+  :key="change.id"
+  v-html="change.value + '\n'"
+/></transition-group></pre>
 </template>
 
 <script>
@@ -17,7 +23,7 @@ export default {
 
   data() {
     return {
-      html: "",
+      html: [],
       sequence: []
     };
   },
@@ -82,7 +88,7 @@ export default {
           id: index
         }));
 
-      this.sequence.push(old, from, to, from, old);
+      this.sequence.push(old, from, to);
     },
 
     sequence() {
@@ -107,9 +113,51 @@ export default {
 
     updateHtml(html) {
       const el = this.$refs.code;
-
       const { transition } = el.style;
       el.style.transition = "";
+
+      // First transition out existing HTML
+      this.transitionIds = this.html
+        .filter(change => !html.find(({ id }) => change.id === id))
+        .map(change => change.id);
+
+      if (this.transitionIds.length > 0) {
+        this.addClass("transition-leave-from");
+      }
+
+      requestAnimationFrame(() => {
+        if (this.$refs.change && this.transitionIds.length > 0) {
+          this.removeClass("transition-leave-from");
+          this.addClass("transition-leave-to");
+
+          this.$refs.change[this.transitionIds[0]].addEventListener(
+            "transitionend",
+            this.handleCodeTransitionEnd.bind(this, html, transition)
+          );
+        } else {
+          this.transitionTo(html, transition);
+        }
+      });
+    },
+
+    handleCodeTransitionEnd(html, transition) {
+      this.removeClass("transition-leave-to");
+      this.addClass("transition-leave-end");
+
+      requestAnimationFrame(() => {
+        this.removeClass("transition-leave-end");
+      });
+
+      this.$refs[this.transitionIds[0][0]].removeEventListener(
+        "transitionend",
+        this.handleCodeTransitionEnd
+      );
+
+      this.transitionTo(html, transition);
+    },
+
+    transitionTo(html, transition) {
+      const el = this.$refs.code;
 
       // Wait until the CSS properties have updated
       requestAnimationFrame(() => {
@@ -131,6 +179,18 @@ export default {
 
     handleTransitionEnd() {
       this.$refs.code.style.height = undefined;
+    },
+
+    addClass(className) {
+      this.transitionIds.forEach(id => {
+        this.$refs[id][0].classList.add(className);
+      });
+    },
+
+    removeClass(className) {
+      this.transitionIds.forEach(id => {
+        this.$refs[id][0].classList.remove(className);
+      });
     }
   }
 };
@@ -138,27 +198,43 @@ export default {
 
 <style>
 .code-container {
-  transition: height 0.7s ease-in-out;
+  transition: height 1s ease-in-out;
   box-sizing: border-box;
 }
 
 .list-item {
   display: block;
-  transition: opacity 0.7s ease-in-out;
+  transition: all 1s ease-in-out;
 }
 
-.list-enter, .list-leave-to
-/* .list-leave-active below version 2.1.8 */ {
-  opacity: 0;
+/* .list-enter, .list-leave-to */
+/* .list-leave-active below version 2.1.8 { */
+/* opacity: 0;
   transform: translateX(30px);
-}
+} */
 
 .list-move {
   opacity: 1;
-  transition: transform 0.7s ease-in-out;
+  transition: transform 1s ease-in-out;
 }
 
 .list-leave-active {
-  display: none;
+  /* display: none; */
+}
+
+.transition-leave-to,
+.transition-leave-from {
+}
+
+.transition-leave-from {
+  background: red;
+}
+
+.transition-leave-to {
+  background: green;
+}
+
+.transition-leave-end {
+  /* display: none; */
 }
 </style>
